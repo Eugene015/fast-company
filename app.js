e3e4bc7e7f8e3d2964b2963e57ecbfd74f858157
Eugene015 <egavriluk@gmail.com>
2023-01-12 12:48:27 +0100

    some fixes

--- app.js
+++ app.js
@@ -1,47 +1,43 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const config = require('config')
-const chalk = require('chalk')
-const cors = require('cors')
-const path = require('path')
-const initDatabase = require('./startUp/initDatabase')
-const routes = require('./routes')
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("config");
+const chalk = require("chalk");
+const cors = require("cors");
+const path = require("path");
+const initDatabase = require("./startUp/initDatabase");
+const routes = require("./routes");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
-app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
-app.use('/api', routes)
+app.use("/api", routes);
 
-const PORT = config.get('port') ?? 8080
+const PORT = config.get("port") ?? 9000;
 
-if (process.env.NODE_ENV === 'production') {
-  app.use('/', express.static(path.join(__dirname, 'client')))
-
-  const indexPath = path.join(__dirname, 'client', 'index.html')
-
-  app.get('*', (req, res) => {
-    res.sendFile(indexPath)
-  })
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.resolve(__dirname, "client", "build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 async function start() {
   try {
-    mongoose.connection.once('open', () => {
-      initDatabase()
-    })
-    await mongoose.connect(config.get('mongoUri'))
-    console.log(chalk.green(`MongoDB connected.`))
-    app.listen(PORT, () =>
+    mongoose.connection.once("open", () => {
+      initDatabase();
+    });
+    await mongoose.connect(config.get("mongoUri"));
+    console.log(chalk.green(`MongoDB connected.`));
+    app.listen(process.env.PORT || 9000, () =>
       console.log(chalk.green(`Server has been started on port ${PORT}...`))
-    )
+    );
   } catch (e) {
-    console.log(chalk.red(e.message))
-    process.exit(1)
+    console.log(chalk.red(e.message));
+    process.exit(1);
   }
 }
 
-start()
-
+start();
